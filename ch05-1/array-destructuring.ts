67ffa8113deba696af7b211b63dc2a1729269cf9
sujin <sujin.k0402@gmail.com>
2021-07-20 23:04:18 +0900

    Added ch05-1

---
+++ ch05-1/array-destructuring.ts
@@ -0,0 +1,3 @@
+let array2: number[] = [1, 2, 3, 4, 5]
+let [first, second, third, ...rest] = array2
+console.log(first, second, third, rest)
---
+++ ch05-1/join-test.ts
@@ -0,0 +1,2 @@
+import { join } from "./join";
+console.log(join(['h', 'e', 'l', 'l', 'o']), join(['h', 'e', 'l', 'l', 'o'], '_'))
